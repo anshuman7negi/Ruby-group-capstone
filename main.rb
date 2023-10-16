11faac935fccf7c95a79fea5c1cfa67f184ae75a
anshuman7negi <anshumannegi777@gmail.com>
2023-10-16 20:48:28 +0530

    added constructor and display option in main file

--- main.rb
+++ main.rb
@@ -1,11 +1,23 @@
 class Main
+
+  def initialize
+    @option = [
+      'can we archived',
+      'move to archived',
+      'exit'
+    ]
+  end
+
   def display_options
     puts 'Welcome to the Catalog of my Things app'
     puts 'Please choose an option:'
-    puts '7 - Exit app'
+    
+    @option.each_with_index do |item,index|
+      puts "#{index+1} => #{item}"
+    end
   end
 
-  def get_choice
-    gets.chomp.to_i
-  end
 end
+
+main = Main.new
+main.display_options
