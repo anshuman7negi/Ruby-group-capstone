4afe40f860a2c926e5128fd3fc2b916128b28f06
anshuman7negi <anshumannegi777@gmail.com>
2023-10-17 14:13:26 +0530

    fixed the constructor

--- books.rb
+++ books.rb
@@ -4,7 +4,7 @@ class Books < Item
   attr_accessor :publisher, :cover_state
 
   def initialize(publisher, cover_state, publish_date, archived: false)
-    super(publish_date,archived: archived)
+    super(publish_date, archived: archived)
     @publisher = publisher
     @cover_state = cover_state
   end
