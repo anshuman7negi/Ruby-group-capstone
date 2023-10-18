5771929a144aa16e904e782e0c08e1f0da7a2699
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-10-18 09:13:13 +0100

    fixed linter errors

--- game.rb
+++ game.rb
@@ -3,7 +3,7 @@ require_relative 'item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(multiplayer, last_played_at, publish_date)
+  def initialize(multiplayer, _last_played_at, _publish_date)
     super(date)
     @multiplayer = multiplayer
     @last_played_at = date
