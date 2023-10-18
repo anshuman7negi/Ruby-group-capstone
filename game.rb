06040b153a6c966ad1ed972a26845f629d417789
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-10-18 09:38:58 +0100

    fix game file according to review

--- game.rb
+++ game.rb
@@ -3,10 +3,10 @@ require_relative 'item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(multiplayer, _last_played_at, _publish_date)
-    super(date)
+  def initialize(multiplayer, last_played_at, publish_date)
+    super(publish_date)
     @multiplayer = multiplayer
-    @last_played_at = date
+    @last_played_at = Date.parse(last_played_at)
   end
 
   def can_be_archived?
