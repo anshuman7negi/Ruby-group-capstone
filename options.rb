62f3cb6cae76d077645c931073f38d3919ce2197
anshuman7negi <anshumannegi777@gmail.com>
2023-10-19 11:07:39 +0530

    fixed linter errors

--- options.rb
+++ options.rb
@@ -1,35 +1,29 @@
 require './app'
 
 class Options
-  def initialize
-    @option = [
-      'List all books',
-      'List all music albums',
-      'List all genres',
-      'List all labels',
-      'Add a book',
-      'Add a music album',
-      'exit'
-    ]
-  end
+  OPTIONS = {
+    list_books: 'List all books',
+    list_albums: 'List all music albums',
+    list_genres: 'List all genres',
+    list_labels: 'List all labels',
+    add_book: 'Add a book',
+    add_album: 'Add a music album',
+    exit: 'exit'
+  }
 
   def display_options
     puts '======Welcome to the Catalog of my Things app======'
     loop do
-      puts "\n"
-      puts 'Please choose an option:'
-
+      puts '\nPlease choose an option:'
       @app = App.new
-
-      @option.each_with_index do |item, index|
-        puts "#{index + 1} => #{item}"
+      OPTIONS.each_with_index do |(_key, value), index|
+        puts "#{index + 1} - #{value}"
       end
       user_option = gets.chomp.to_i
       operation(user_option)
     end
   end
 
-  # rubocop:disable Metrics/CyclomaticComplexity
   def operation(user_option)
     case user_option
     when 1 then @app.books_list
@@ -43,5 +37,4 @@ class Options
       puts '=======invalid option==========='
     end
   end
-  # rubocop:enable Metrics/CyclomaticComplexity
 end
