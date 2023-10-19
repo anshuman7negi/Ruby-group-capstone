3418f0303c7fc751e29a14a5eec672904369205d
anshuman7negi <anshumannegi777@gmail.com>
2023-10-19 11:20:34 +0530

    fixed linter errors

--- options.rb
+++ options.rb
@@ -9,15 +9,16 @@ class Options
     add_book: ->(app) { app.add_book },
     add_album: ->(app) { app.add_music_album },
     exit: ->(_) { exit }
-  }
+  }.freeze
 
   def display_options
     puts '======Welcome to the Catalog of my Things app======'
     loop do
-      puts '\nPlease choose an option:'
+      puts "\nPlease choose an option:"
       @app = App.new
-      OPTIONS.each_with_index do |(key, _), index|
-        puts "#{index + 1} - #{OPTIONS[key]}"
+      OPTIONS.each_with_index do |(key, _description), index|
+        formatted_key = key.to_s.split('_').map(&:capitalize).join(' ')
+        puts "#{index + 1} - #{formatted_key}"
       end
       user_option = gets.chomp.to_i
       operation(user_option)
