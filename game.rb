847a9f3eea94c3f319a2eb03572ab9a3ff924b89
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-10-20 08:26:36 +0100

    preserve game data in json file.

--- game.rb
+++ game.rb
@@ -12,4 +12,19 @@ class Game < Item
   def can_be_archived?
     super && (Time.now.year - @last_played_at.year) > 2
   end
+  def to_hash
+    {
+      publish_date: @publish_date.strftime('%Y-%m-%d'),
+      multiplayer: @multiplayer,
+      last_played_at: @last_played_at.strftime('%Y-%m-%d')
+    }
+  end
+
+  def self.from_hash(hash)
+    new(
+      hash['publish_date'],
+      hash['multiplayer'],
+      hash['last_played_at']
+    )
+  end
 end
