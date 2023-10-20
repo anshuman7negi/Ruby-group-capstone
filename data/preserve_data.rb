bfc6c12b39799cc2e8dd08904f87b79eb0274469
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-10-20 09:56:37 +0100

    update data files

--- data/preserve_data.rb
+++ data/preserve_data.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
-
 require 'json'
-
 def save_book(data, filename)
   data_array = data.map(&:to_h)
   File.write(filename, JSON.generate(data_array))
 end
-
 def save_label(data, filename)
   data_array = data.map(&:to_h)
   File.write(filename, JSON.generate(data_array))
 end
-
 def save_musicalbum(data, filename)
   data_array = data.map(&:to_h)
   File.write(filename, JSON.generate(data_array))
 end
-
 def save_genre(data, filename)
   data_array = data.map(&:to_h)
   File.write(filename, JSON.generate(data_array))
 end
-
+def save_game(data, filename)
+  data_array = data.map(&:to_h)
+  File.write(filename, JSON.generate(data_array))
+end
+def save_author(data, filename)
+  data_array = data.map(&:to_h)
+  File.write(filename, JSON.generate(data_array))
+end
 def load_data(filename)
   if File.exist?(filename)
     data = File.read(filename)
@@ -30,3 +32,10 @@ def load_data(filename)
     []
   end
 end
+
+
+
+
+
+
+
