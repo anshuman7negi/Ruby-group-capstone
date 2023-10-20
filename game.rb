494ccb2312ff7bdbf1beaafd2a38b87d8c076953
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-10-20 09:55:13 +0100

    update game.rb file

--- game.rb
+++ game.rb
@@ -1,20 +1,15 @@
 # frozen_string_literal: true
-
 require_relative 'item'
-
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
-
   def initialize(multiplayer, last_played_at, publish_date)
     super(publish_date)
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
   end
-
   def can_be_archived?
     super && (Time.now.year - @last_played_at.year) > 2
   end
-
   def to_hash
     {
       publish_date: @publish_date.strftime('%Y-%m-%d'),
@@ -22,7 +17,6 @@ class Game < Item
       last_played_at: @last_played_at.strftime('%Y-%m-%d')
     }
   end
-
   def self.from_hash(hash)
     new(
       hash['publish_date'],
@@ -30,4 +24,18 @@ class Game < Item
       hash['last_played_at']
     )
   end
+  def to_h
+    {
+      'multiplayer' => @multiplayer,
+      'last_played_at' => @last_played_at,
+      'publish_date' => @publish_date
+    }
+  end
 end
+
+
+
+
+
+
+
