3375fead9d0bc6aa251d7d1506c0383a8c84c52e
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-10-20 09:36:37 +0100

    update game and author modules

--- modules/create_game.rb
+++ modules/create_game.rb
@@ -1,46 +1,21 @@
 # frozen_string_literal: true
-
 require './game'
 require './author'
 require './item'
-require 'json'
-
-def save_game(filename)
-  data = @games.map { |game| game.to_hash.merge(title: game.label) }
-  save_data(filename, data, 'Game')
-end
-
-def save_authors(filename)
-  data = @authors.map(&:to_hash)
-  save_data(filename, data, 'Author')
-end
-
-def load_authors(filename)
-  return unless File.exist?(filename)
-
-  data = File.read(filename)
-  author_data = JSON.parse(data)
-  @authors = author_data.map do |author_data_item|
-    author = Author.from_hash(author_data_item)
-    author
+require './modules/create_author'
+module CreateGame
+  include CreateAuthor
+  def add_game
+    puts 'Is this a multiplayer game (true/false) ?'
+    multiplayer = gets.chomp.to_s
+    puts 'Enter the last played game date (yyyy-mm-dd):'
+    last_played_at = gets.chomp.to_s
+    puts 'Enter the game publish date (yyyy-mm-dd):'
+    publish_date = gets.chomp
+    game = Game.new(multiplayer, last_played_at, publish_date)
+    @games.push(game)
+    save_book(@games, './data/game.json')
+    puts '=====Game added successfully====='
+    author_info(game)
   end
-  puts "Author data loaded from #{filename}"
-end
-
-def load_games(filename)
-  return unless File.exist?(filename)
-
-  data = File.read(filename)
-  game_data = JSON.parse(data)
-  @games = game_data.map do |game_data_item|
-    game = Game.from_hash(game_data_item)
-    game.label = game_data_item['title']
-    game
-  end
-  puts "Game data loaded from #{filename}"
-end
-
-def save_data(filename, data, data_type)
-  File.write(filename, JSON.pretty_generate(data))
-  puts "#{data_type} data saved to #{filename}"
 end
